9f5fa5d2701de9214ea3f8bcd7a8e74b7cdef282
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-02 20:06:59 +0800

    Create Days01.java

---
+++ Days01.java
@@ -0,0 +1,19 @@
+/**
+ * Contoh penggunaan komentar dalam Java.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // nama gw
+        String nama = "Fauzan";
+
+        /*
+         * halo nama saya Fauzan.
+         * asal malunda, bukan malu nda.
+         * alumni UPTD SMAN 1 MALUNDA.
+         */
+        System.out.println("Halo, " + nama);
+
+    }
+}
