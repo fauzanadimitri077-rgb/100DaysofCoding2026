c29fa10d2aa01c9e782264b1c1c2d464a5493892
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-03 20:49:42 +0800

    Create Day02.java

---
+++ Day02.java
@@ -0,0 +1,17 @@
+public class day 2 :Biodata {
+    public static void main(String[] args) {
+
+        // Menggunakan println()
+        System.out.println("=== BIODATA DIRI ===");
+        System.out.println("Nama       : Fauzan Adi Mitri");
+        System.out.println("Prodi      : Informatika");
+        
+        // menggunakan print ()
+        System.out.print("alasan masuk isc :");
+        system.out.print("saya ingin jago dalam hal ngoding jadi saya rasa di sini tempatnya");
+        
+        int umur = 18;
+        
+        System.out.printf("halo umur saya tahun ini adalah %d.", umur);
+    }
+}
