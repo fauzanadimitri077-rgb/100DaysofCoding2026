66595e049266dba0e9f7ca565ae4822933bedb5d
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-04 21:48:36 +0800

    Create Day 03.java

---
+++ Day 03.java
@@ -0,0 +1,26 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// \n digunakan untuk menambahkan baris baru 
+		System.out.print("fauzan adi mitri");
+		System.out.print("\ndari informatika");
+		
+		// \t digunakan untuk menambahkan tab
+		System.out.println("\nfauzan adi\tmitri");
+		
+		// \"Digunakan jika ingin menampilkan tanda " di
+		System.out.println("fauzan adi \"mitri\"");
+		
+		// \' digunkan jika ingin menampilkan tanda '  
+		System.out.println("fauzan adi \'mitri\'");
+		
+		// \\ Digunakan untuk menampilkan \
+		System.out.println("fauzan adi mitri\\ucang");
+		
+		// \b Digunakan untuk menghapus/menggeser satu karakter ke belakang dalam konteks tertentu.
+		System.out.println("fauzan adi \bmitri ");
+		
+		// \f igunakan untuk form feed, dan saat ini cukup jarang digunakan.
+		System.out.println("fauzan adi\fmitri ");
+	}
