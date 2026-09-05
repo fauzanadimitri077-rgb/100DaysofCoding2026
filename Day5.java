8876887d451adabc8853a7187e0c4e6b3d252f8f
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-06 01:07:08 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,18 @@
+public class Pecahan {
+    public static void main(String[] args) {
+
+        // Tipe data double
+        double tinggi = 170.5;
+        double berat = 60.75;
+
+        // Tipe data float
+        float nilai = 89.5f;
+        float suhu = 36.5f;
+
+        System.out.println("Tinggi badan : " + tinggi + " cm");
+        System.out.println("Berat badan  : " + berat + " kg");
+
+        System.out.println("Nilai        : " + nilai);
+        System.out.println("Suhu         : " + suhu + " °C");
+    }
+}
