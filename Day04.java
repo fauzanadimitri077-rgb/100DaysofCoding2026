f1cc3bc297db6d76002ebaf9537b46c006e2d982
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-05 22:23:57 +0800

    Create Day04.java

---
+++ Day04.java
@@ -0,0 +1,23 @@
+public class BilanganBulat {
+    public static void main(String[] args) {
+
+        // Tipe data byte
+        byte umur = 18;
+
+        // Tipe data short
+        short jumlahSiswa = 500;
+
+        // Tipe data int
+        int harga = 1500000;
+
+        // Tipe data long
+        long jumlahPenduduk = 8000000000L;
+
+        // Menampilkan data
+        System.out.println("=== TIPE DATA BILANGAN BULAT ===");
+        System.out.println("Byte           : " + umur);
+        System.out.println("Short          : " + jumlahSiswa);
+        System.out.println("Int            : " + harga);
+        System.out.println("Long           : " + jumlahPenduduk);
+    }
+}
