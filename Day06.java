13bf6eb383cdad7557c19b8283e070ffd78f0d0e
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-07 21:22:23 +0800

    Create Day06.java

---
+++ Day06.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		char jenisKelamin = 'L';
+		boolean data = 6>2 ;
+		System.out.println(jenisKelamin);
+		System.out.println(data);
+		
+	}
+}
