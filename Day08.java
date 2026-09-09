18a612717e0e98b99377592abe469147266e7331
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-09 21:30:28 +0800

    Create Day08.java

---
+++ Day08.java
@@ -0,0 +1,16 @@
+public class Variabel {
+    public static void main(String[] args) {
+
+        // Deklarasi
+        int umur;
+
+        // Inisialisasi
+        umur = 18;
+
+        System.out.println(umur);
+
+        // Update variabel
+
+        System.out.println( 1 + umur);
+    }
+}
