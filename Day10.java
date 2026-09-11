9fdbadbc222ed883aec4a723aeff5f47849e4ccf
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-11 21:36:41 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,14 @@
+public class BatasTipeData {
+    public static void main(String[] args) {
+
+        System.out.println("=== BATAS TIPE DATA JAVA ===");
+
+        System.out.println("byte  : " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+        System.out.println("short : " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
+        System.out.println("int   : " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+        System.out.println("long  : " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+
+        System.out.println("float  : " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+        System.out.println("double : " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+    }
+}
