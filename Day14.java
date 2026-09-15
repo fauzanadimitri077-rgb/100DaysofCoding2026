1db93f5e57bb0b5620acb716df4efc34b6b2d6b2
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-15 16:46:27 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+      int angka1 = 15 ;
+      int angka2 = 5 ;
+      int hasil1 = angka1 + angka2 ;
+      int hasil2 = angka1 - angka2 ;
+
+      System.out.println("Hasil :" + hasil1 );
+      System.out.println("Hasil :" + hasil2);
+
+      
+
+    }
+}
