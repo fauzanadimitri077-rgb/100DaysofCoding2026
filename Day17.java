b1bca67bee685ebd9aee49499a6a9f715b0b0520
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-18 22:15:57 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+      int angka = 10;
+      angka += 5;
+      System.out.println(angka);
+      angka -= 5;
+      System.out.println(angka);
+      angka *= 5;
+      System.out.println(angka);
+      angka /= 5;
+      System.out.println(angka);
+      angka %= 3;
+      System.out.println(angka);
+      
+
+    }
+}
