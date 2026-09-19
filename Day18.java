86249aa26745d76abe921941451eddd93203a32b
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-19 20:37:08 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,17 @@
+public class KonversiOtomatis {
+    public static void main(String[] args) {
+      byte A = 10;
+      short B = A;
+      int C = B;
+      long D = C;
+      float E = D;
+      double F = E;
+      System.out.println("byte :" + A);
+      System.out.println("short :" + B);
+      System.out.println("int :" + C);
+      System.out.println("long :" + D);
+      System.out.println("float :" + E);
+      System.out.println("double :" + F);
+      
+    }
+}
