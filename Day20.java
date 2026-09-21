259bae3074027d64334d068a7f789a4468a0dfe1
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-21 23:10:41 +0800

    Update Day20.java

--- Day20.java
+++ Day20.java
@@ -1,16 +1,7 @@
 public class main {
     public static void main(String[] args) {
-      String bulat = "123";
-      String desimal = "11.1";
-      String benar = "true";
-      
-      int a = Integer.parseInt(bulat);
-      double b = Double.parseDouble(desimal);
-      boolean c = Boolean.parseBoolean(benar);
-
-      System.out.println(a);
-      System.out.println(b);
-      System.out.println(c);
+        int angka = 300;
+        String angka2 = String.valueOf(angka);
+        System.out.println("Hasil   : " + angka2 );
 
     }
-}
