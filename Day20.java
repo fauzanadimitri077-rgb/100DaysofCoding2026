f98dc63329fe1646b866c91790da73e789ab1c8d
Fauzan_-! Ucang <fauzanadimitri077@gmail.com>
2026-09-21 22:49:36 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+      String bulat = "123";
+      String desimal = "11.1";
+      String benar = "true";
+      
+      int a = Integer.parseInt(bulat);
+      double b = Double.parseDouble(desimal);
+      boolean c = Boolean.parseBoolean(benar);
+
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+
+    }
+}
